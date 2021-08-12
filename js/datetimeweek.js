219d4b1e73cbf2d6a31ccc0d00d217387c71d46a
zedpentium <zedpentium@gmail.com>
2021-08-12 14:28:51 +0200

    Exercise 10 to 17 is done

--- js/datetimeweek.js
+++ js/datetimeweek.js
@@ -1,15 +1,15 @@
 /*!
-  * Eric R javascrip looks
-*/
+ * Eric R javascrip looks
+ */
 var myContent = document.getElementById("wrapper");
 myContent.style.backgroundColor = "darkgray";
 
 
 /* update time every second */
-setInterval(function (){ // Exercise 1
+setInterval(function () { // Exercise 1
   var todayDate = new Date();
-    /*var format = "YYYY-MMM-DD DDD";
-    dateConvert(todayDate,format)*/
+  /*var format = "YYYY-MMM-DD DDD";
+  dateConvert(todayDate,format)*/
   var weekday = new Array(7);
   weekday[0] = "Sunday";
   weekday[1] = "Monday";
@@ -18,12 +18,12 @@ setInterval(function (){ // Exercise 1
   weekday[4] = "Thursday";
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
-  
+
   var dayNameResult = weekday[todayDate.getDay()];
-  
+
   var fulldatetime = dayNameResult + ", " + todayDate.toLocaleString();
   document.getElementById('datetimeweek').innerHTML = fulldatetime;
-  }, 1000);
+}, 1000);
 
 
 
@@ -37,7 +37,7 @@ function checkIfLeapYear() { // Exercise 2 & 3
     yesNoAnswer = " IS a leapyear.";
   } else {
     yesNoAnswer = " is NOT leapyear.";
-  } 
+  }
 
   var resultofyearcheck = "The year: " + userinputyear + yesNoAnswer;
   document.getElementById('resultofcheck').innerHTML = " " + resultofyearcheck;
@@ -45,14 +45,14 @@ function checkIfLeapYear() { // Exercise 2 & 3
 
   var firstJanYear = new Date(userinputyear, 0, 1);
 
-  if ( firstJanYear.getDay() === 0 ) {
+  if (firstJanYear.getDay() === 0) {
     isFirstJan = " IS a Sunday";
   } else {
     isFirstJan = " is NOT a Sunday";
   }
 
-var isFirstjanOnSunday = "1st January of " + userinputyear + isFirstJan;
-document.getElementById('resultOfFirstJanCheck').innerHTML = " " + isFirstjanOnSunday;
+  var isFirstjanOnSunday = "1st January of " + userinputyear + isFirstJan;
+  document.getElementById('resultOfFirstJanCheck').innerHTML = " " + isFirstjanOnSunday;
 }
 
 
@@ -66,8 +66,8 @@ function checkGuessedNr() { // Exercise 4
   if (userinputGuess == theRndNr) {
     isGuessCorrect = " is CORRECT, Good Job! ;)";
   } else {
-    isGuessCorrect = " is wrong. The number " + theRndNr +  " did not match.";
-  } 
+    isGuessCorrect = " is wrong. The number " + theRndNr + " did not match.";
+  }
 
   var resultofguesscheck = "Your guess: " + userinputGuess + isGuessCorrect;
   document.getElementById('resultofguess').innerHTML = " " + resultofguesscheck;
@@ -76,16 +76,15 @@ function checkGuessedNr() { // Exercise 4
 
 function daysTilXmasNow() { // Exercise 5
 
-var dateNow = new Date();
-var daysuntilXmas = new Date(dateNow.getFullYear(), 11, 24);
+  var dateNow = new Date();
+  var daysuntilXmas = new Date(dateNow.getFullYear(), 11, 24);
 
-if (dateNow.getMonth()==11 && dateNow.getDate()>24) 
-{
-  daysuntilXmas.setFullYear(daysuntilXmas.getFullYear()+1); 
-}  
-var one_day=1000*60*60*24;
+  if (dateNow.getMonth() == 11 && dateNow.getDate() > 24) {
+    daysuntilXmas.setFullYear(daysuntilXmas.getFullYear() + 1);
+  }
+  var one_day = 1000 * 60 * 60 * 24;
 
-  daysleftxmas = (Math.ceil((daysuntilXmas.getTime()-dateNow.getTime())/(one_day))) + " days";
+  daysleftxmas = (Math.ceil((daysuntilXmas.getTime() - dateNow.getTime()) / (one_day))) + " days";
   document.getElementById('showdaystoxmas').innerHTML = daysleftxmas;
 }
 
@@ -96,9 +95,9 @@ function reverseUserInput() { // Exercise 6
 
   var newReversedString = "";
 
-    for (var ind = userinputStr.length - 1; ind >= 0; ind--) {
-      newReversedString += userinputStr[ind];
-    }
+  for (var ind = userinputStr.length - 1; ind >= 0; ind--) {
+    newReversedString += userinputStr[ind];
+  }
 
   document.getElementById('reversedResulOfInput').innerHTML = "Heres input in reverse order: " + newReversedString;
 }
@@ -109,29 +108,29 @@ function textmanipulUserInput() { // Exercise 7
   var userinputStr = document.getElementById("inputUserText").value;
   var array1 = [];
 
-    for (var x = 0, y=1; x < userinputStr.length; x++,y++) {
-     array1[x]=userinputStr.substring(x, y);
-      }
+  for (var x = 0, y = 1; x < userinputStr.length; x++, y++) {
+    array1[x] = userinputStr.substring(x, y);
+  }
 
   var combi = [];
-  var temp= "";
+  var temp = "";
   var slent = Math.pow(2, array1.length);
-  
-  for (var i = 0; i < slent ; i++) {
-      temp= "";
-      for (var j=0;j<array1.length;j++) {
-          if ((i & Math.pow(2,j))){ 
-              temp += array1[j];
-          }
+
+  for (var i = 0; i < slent; i++) {
+    temp = "";
+    for (var j = 0; j < array1.length; j++) {
+      if ((i & Math.pow(2, j))) {
+        temp += array1[j];
       }
-      if (temp !== "") {
-          combi.push(temp);
-      }
+    }
+    if (temp !== "") {
+      combi.push(temp);
+    }
   }
-    //console.log(combi.join("\n"));
-    document.getElementById('textmanipulResult').textContent = "Heres all combinations of the text: " + combi.join(", ");
-  }
-  
+  //console.log(combi.join("\n"));
+  document.getElementById('textmanipulResult').textContent = "Heres all combinations of the text: " + combi.join(", ");
+}
+
 
 /* function getArrayMutations(arr, perms = [], len = arr.length) { // all permutation of a array
   if (len === 1) perms.push(arr.slice(0))
@@ -151,43 +150,260 @@ function textmanipulUserInput() { // Exercise 7
 function textmanipulUserInput2() { // Exercise 8
   var userinputStr = document.getElementById("inputUserText2").value.toLowerCase();
   var array1 = [];
-  
-    for (var x = 0, y=1; x < userinputStr.length; x++,y++) {
-     array1[x]=userinputStr.substring(x, y);
-      }
 
-    document.getElementById('textmanipulResult2').textContent = "Input of letters/word, sorted letters alphabetically: " + array1.sort();
+  for (var x = 0, y = 1; x < userinputStr.length; x++, y++) {
+    array1[x] = userinputStr.substring(x, y);
   }
 
+  document.getElementById('textmanipulResult2').textContent = "Input of letters/word, sorted letters alphabetically: " + array1.sort();
+}
 
-  function textmanipulUserInput3() { // Exercise 9
-    var userinputValue = document.getElementById("inputUserText3").value;
-    var inputArray = [];
-    
-      for (var x = 0, y=1; x < userinputValue.length; x++,y++) {
-        inputArray[x]=userinputValue.substring(x, y);
-        }
 
-        inputArray[0] = inputArray[0].toUpperCase()
+function textmanipulUserInput3() { // Exercise 9
+  var userinputValue = document.getElementById("inputUserText3").value;
+  var inputArray = [];
 
-      for (let index = 0; index < inputArray.length; index++) {
-        const elementInArray = inputArray[index];
+  for (var x = 0, y = 1; x < userinputValue.length; x++, y++) {
+    inputArray[x] = userinputValue.substring(x, y);
+  }
 
-        if (elementInArray == " ") {
-          inputArray[index + 1] = inputArray[index + 1].toUpperCase()
-        }
-        
-      }
+  inputArray[0] = inputArray[0].toUpperCase()
 
-      document.getElementById('textmanipulResult3').textContent = "First letter uppercase in each word: " + inputArray.join("");
+  for (let index = 0; index < inputArray.length; index++) {
+    const elementInArray = inputArray[index];
+
+    if (elementInArray == " ") {
+      inputArray[index + 1] = inputArray[index + 1].toUpperCase()
     }
 
+  }
 
-    function primeNrUserInput() { // Exercise 10
-      var userinputValue = document.getElementById("inputNumberToCheck").value;
-      
-  
-        document.getElementById('primeNrResult3').textContent = "First letter uppercase in each word: " + inputArray.join("");
+  document.getElementById('textmanipulResult3').textContent = "First letter is now Upperase in each word: " + inputArray.join("");
+}
+
+
+function primeNrUserInput() { // Exercise 10
+  var userInput = document.getElementById("inputNumberToCheck").value;
+  var isItaPrimeNrText;
+
+  if (getTrueOrFalsePrime(userInput)) {
+    isItaPrimeNrText = "Yes, " + userInput + " IS a Prime number.";
+  } else {
+    isItaPrimeNrText = "No, " + userInput + " is NOT a Prime number.";
+  }
+  document.getElementById('primeNrResult').textContent = isItaPrimeNrText;
+} //function endtag
+
+function getTrueOrFalsePrime(isItPrime) { // separate check if primeNumber function, then true or false)
+  if (isItPrime == 1) {
+    return false;
+  } else if (isItPrime == 2) {
+    return true;
+  } else {
+    for (var x = 2; x < isItPrime; x++) {
+      if (isItPrime % x == 0) {
+        return false;
+      } else {
+        return true;
       }
+    } // for loop endtag
+  } // else endtag
+} // function endtag
 
 
+function checkValueUserInput(userInputParam) { // Exercise 11
+
+  var typeOfValue;
+
+  if (userInputParam === "")
+    typeOfValue = "";
+  else if (userInputParam === "true" || userInputParam === "false")
+    typeOfValue = "boolean";
+  else if (!Number.isNaN(Number(userInputParam)))
+    typeOfValue = "number";
+  else
+    typeOfValue = typeof userInputParam;
+
+  console.log(typeOfValue);
+
+  document.getElementById('valueTypeOF').textContent = "This is the input Type: " + typeOfValue;
+}
+
+
+
+function checkOddAndEvenUserInput() { // Exercise 12
+
+  var fromNr = 0;
+  var toNR = 15;
+  var outputOddEven = "";
+  var responseOut ="";
+
+for (let index = fromNr; index < toNR; index++) {
+
+  if (index % 2 == 0) {
+    responseOut = `${index} is EVEN`;
+  } else {
+    responseOut = `${index} is ODD`;
+  }
+
+  outputOddEven += responseOut + " ,";
+  
+} // for loop endtag
+
+  document.getElementById('oddAndEvenResult').textContent = outputOddEven;
+
+} // function endtag
+
+
+
+function tenHappyNumbers() { // Exercise 13
+
+  var outputhappy = "";
+
+  var cnt = 10;
+  var num = 1;
+
+  while(cnt-- > 0) 
+  {
+      while(!happy_number(num))
+          num++ ;
+          outputhappy += + num + ", ";
+  
+      num++ ;
+  }
+  document.getElementById('outputhappyResult').textContent = "10 first happy numbers: " + outputhappy;
+
+} // function endtag
+  
+function happy_number(num) { // Exercise 13 - Happy number calc function 
+  var m, n ;
+    var c = [] ;
+ 
+    while(num != 1 && c[num] !== true) 
+    {
+        c[num] = true ;
+        m = 0 ;
+        while (num > 0) {
+            n = num % 10 ;
+            m += n * n ;
+            num = (num  - n) / 10 ;
+        }
+        num = m ;
+    }
+    return (num == 1) ;
+}
+ 
+
+
+function joinArrayToString() { // Exercise 14
+
+var workDaysArray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+var outputOddEven = "";
+
+for (let index = 0; index < workDaysArray.length; index++) {
+  outputOddEven += workDaysArray[index] + ", ";
+ } // for loop endtag
+
+  document.getElementById('joinArrayToStringResult').textContent = outputOddEven + " are workdays.";
+
+} // function endtag
+
+
+
+function sortNrArray() { // Exercise 15
+
+  var nrArrayInput = document.getElementById("inputSortNrArray").value;
+  const theNrArray = nrArrayInput.split(", ");
+  const sortedNrArray = theNrArray.sort(function(a, b){return a - b});
+  
+  var outputNrArray = "";
+  
+  for (let index = 0; index < sortedNrArray.length; index++) {
+    outputNrArray += sortedNrArray[index] + ", ";
+   } // for loop endtag
+  
+    document.getElementById('sortNrArrayResult').textContent = outputNrArray;
+  
+  } // function endtag
+
+
+
+  function RemoveDuplicates() { // Exercise 16
+
+    var nrArrayInput = document.getElementById("inputarrayRemoveDuplicates").value;
+    var theNrArray = nrArrayInput.split(", ");
+    var theDuplicateIndex = [];
+    var theFounddups = [];
+    
+    for (var index = 0; index < theNrArray.length; index++) { // 1st for-loop
+
+      for (var indUp = index + 1; indUp < theNrArray.length; indUp++) { // 2nd for-loop
+        if (theNrArray[index] === theNrArray[indUp]) {
+          theDuplicateIndex.push(indUp);
+          //console.log("Dups index: " + indUp + ", dups value: " + theNrArray[indUp]);
+          theFounddups.push(theNrArray[indUp]);
+        } 
+
+        //console.log("Index: " + index + ", Index value: " + theNrArray[index]);
+      } // 2nd for-loop endtag
+
+     
+      for (let dupind = 0; dupind < theDuplicateIndex.length; dupind++) { // 3rd for-loop
+        theNrArray.splice(theDuplicateIndex[dupind],1) // removes duplicates found in 2nd for-loop
+      } // 3rd for-loop endtag
+
+      theDuplicateIndex.length = 0; // to empty array for next passes
+
+     } // 1st for-loop endtag
+
+     var outputNrArray = "Theese are duplicates " + theFounddups.join(", ") + ", and here is the array without any duplicates: "  + theNrArray.join(", ");
+
+      document.getElementById('RemoveDuplicatesResult').textContent = outputNrArray;
+    
+    } // function endtag
+
+
+    function arrayRemoveNullZero() { // Exercise 17
+
+      var nrArrayInput = document.getElementById("inputarrayRemoveNullZero").value;
+      var theNrArray = nrArrayInput.split(", ");
+      var theNullZeros = [];
+      var theFoundNullZeros = [];
+      //var willBeUniqueNrArray = theNrArray; // Copies the first array fully, to later remove duplicates
+      //const sortedNrArray = theNrArray.sort(function(a, b){return a - b});
+      
+      // var outputNrArray = "";
+      
+      for (let index = 0; index < theNrArray.length; index++) {
+
+        if (theNrArray[index] === "null" || theNrArray[index] === "0") {
+          theNullZeros.push(index);
+          // console.log("Dups index: " + index + ", dups value: " + theNrArray[index]);
+          theFoundNullZeros.push(theNrArray[index]);
+          
+        } 
+        // console.log("Index: " + index + ", Index value: " + theNrArray[index]);
+  
+       } // for-loop endtag
+
+       for (let dupind = theNullZeros.length - 1; dupind >= 0 ; dupind--) {
+        theNrArray.splice(theNullZeros[dupind],1) // removes "null" and "0" found in 2nd for-loop
+       } 
+       
+       // theNullZeros.length = 0;
+
+       var outputNrArray = "Found : " + theFoundNullZeros.join(", ") + " ---- and here is the array without 'null' or '0' (because inputfield is set to text. IF set to number it can not accept null): "  + theNrArray.join(", ");
+  
+        document.getElementById('arrayRemoveNullZeroResult').innerHTML = outputNrArray + "<br>" + "Vad är fångat index för 'null' & '0': " + theNullZeros;
+      
+      } // function endtag
+  
+
+      function daysInMonth() { // Exercise 18
+        var monthInput = document.getElementById("inputMonth").value;
+        var yearInput = document.getElementById("inputYear").value;
+
+
+
+        document.getElementById('daysInMonthResult').innerHTML = outputNrArray;
+      }
